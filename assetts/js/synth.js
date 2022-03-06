0159778e113bd06bb15d76294fc83629137708b3
Cintakins <jacintalerwill@hotmail.com>
2022-03-06 21:27:05 +0000

    enabled notes to continue playing onmousedown and stop onmouseup. Created key-color changes on mouse events.

--- assetts/js/synth.js
+++ assetts/js/synth.js
@@ -1,23 +1,30 @@
 const synth = new Tone.Synth().toDestination();
 const now = Tone.now();
 
-// linking the sounds to keys
-// var c = document.getElementById('#c');
-
-// function playC() { 
-//     synth.triggerAttackRelease('c4', '8n')
-// };
-
-const key = document.getElementsByClassName("key");
+var key = document.getElementsByClassName("key");
 
 for (i = 0; i < key.length; i++){
-    key[i].addEventListener("click", playNote); 
+    key[i].addEventListener("mousedown", playNote); 
 }
+
 function playNote(event) {
-    let note = this.getAttribute("data-note");
-    synth.triggerAttackRelease(`${note}4`, "8n");
+    console.log("play note");
+    Tone.start;
+    var note = this.getAttribute("data-note");
+    synth.triggerAttack(`${note}4`, now);
+    if (this.classList.contains("white-key")) {
+        console.log("white Key");
+    }
+    else {
+        console.log("black key")
+    }
+    this.style.background = this.classList.contains("white-key") ? "aqua" : "rgb(218, 96, 223)"
+    this.addEventListener("mouseup", stopNote); 
 };
-// function stopNote(event) {
-//     let note = document.getAttribute("data-note");
-//     synth.triggerRelease(`${note}4`, now);
-// }
+
+function stopNote() {
+    console.log("stop note");
+    var note = this.getAttribute("data-note")
+    synth.triggerRelease(now);
+    this.style.background = this.classList.contains("white-key") ? "whitesmoke" : "rgb(122, 43, 226)"
+}
