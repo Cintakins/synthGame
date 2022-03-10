b0e726b3f32c8198881df2997d9bae08f4c56f50
Cintakins <jacintalerwill@hotmail.com>
2022-03-10 13:39:16 +0000

    played around with alert messages and reorganising code. Not much progress on issues

--- assetts/js/synth.js
+++ assetts/js/synth.js
@@ -6,8 +6,9 @@ const now = Tone.now();
 // Adding variables
 var key = document.getElementsByClassName("key");
 var tones = [];
-let currentRandomNote = '';
-currentPlayNote = '';
+// let currentRandomNote = '';
+let note = '';
+// let synthKey = '';
 for (i = 0; i < key.length; i++){
     let dataNote = key[i].getAttribute("data-note");
     tones.push(dataNote);
@@ -38,30 +39,10 @@ for (i = 0; i < key.length; i++){
 // playNote function (starts tone, changes colours and listens for mousup event)
 function playNote() {
     Tone.start;
-    let note = this.getAttribute("data-note");
+    var note = this.getAttribute("data-note");
+    // var synthKey = this
     if (currentRandomNote) {
-        let correctNote = currentRandomNote === note;
-        synth.triggerAttack(`${note}4`, now);
-        console.log("played game")
-        if (correctNote) {
-            this.style.background = "green"
-        } else {
-            document.getElementById(`${note}`).style.background = "red";
-            document.getElementById(`${currentRandomNote}`).style.background = "green";
-            setTimeout(() => {
-                document.getElementById(`${currentRandomNote}`).style.background = document.getElementById(`${currentRandomNote}`).classList.contains("white-key") ? "whitesmoke" : "rgb(122, 43, 226)"
-                // resetRandomNote();
-            }, 1500);
-        }
-        // this.style.background = correctNote ? "green" : "red" 
-        this.addEventListener("mouseup", () => {
-            synth.triggerRelease(now);
-            this.style.background = this.classList.contains("white-key") ? "whitesmoke" : "rgb(122, 43, 226)"
-        //     setTimeout(function() {
-        //         correctNote ? alert("Well done you got it right!") : alert("Whoops! Keep practicing!")
-        //         resetRandomNote();
-        // }, 250);
-        });
+        playGame(note, currentRandomNote);
     }
      else {
         synth.triggerAttack(`${note}4`, now);
@@ -70,6 +51,62 @@ function playNote() {
         this.addEventListener("mouseup", stopNote); 
     }
 };
+
+// function for playing the game
+function playGame(currentRandomNote, note){
+    let keyPlayed = document.getElementById(`${note}`);
+    let correctNote = currentRandomNote === note;
+        synth.triggerAttack(`${note}4`, now);
+        console.log("played game")
+
+        var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+        if (correctNote) {
+            keyPlayed.style.background = "green"
+
+            function alert(message, type) {
+                var wrapper = document.createElement('div');
+                wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+                alertPlaceholder.append(wrapper);
+                }
+            // if (correctNote) {
+            //    this.addEventListener('click', function () {
+               alert('Well done!', 'success');
+               setTimeout(() => {
+                   alertPlaceholder.remove();
+               }, 2000)
+            // })
+
+        } 
+        else {
+            keyPlayed.style.background = "red";
+            document.getElementById(`${currentRandomNote}`).style.background = "green";
+            setTimeout(() => {
+                document.getElementById(`${currentRandomNote}`).style.background = document.getElementById(`${currentRandomNote}`).classList.contains("white-key") ? "whitesmoke" : "rgb(122, 43, 226)"
+                // resetRandomNote();
+            }, 1500);
+            
+            function alert(message, type) {
+                var wrapper = document.createElement('div');
+                wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+                alertPlaceholder.append(wrapper);
+                }
+            // if (!correctNote) {
+            //    this.addEventListener('click', function () {
+               alert('Whoops! Play again', 'danger');
+               setTimeout(() => {
+                   alertPlaceholder.remove();
+               }, 2000)
+            // })
+            // }
+        }
+        // this.style.background = correctNote ? "green" : "red" 
+        keyPlayed.addEventListener("mouseup", () => {
+            synth.triggerRelease(now);
+            keyPlayed.style.background = keyPlayed.classList.contains("white-key") ? "whitesmoke" : "rgb(122, 43, 226)"
+        });
+    }
+// }
 
 // stopNote function stops the tone and reverts keys back to original colour
 function stopNote() {
